38f198402d5398ad8501c08c2004d73709245413
Teta_Lena <tetalenaa@gmail.com>
2023-06-14 08:16:16 +0200

    welcome screen and login setup

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+const Login = () => {
+  return (
+    <View className="mt-3">
+      <View></View>
+      <View></View>
+    </View>
+  );
+};
+
+export default Login;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 const LoginScreen = () => {
   return (
     <View className="container w-full h-full">
-      <View className="h-full bg-[#f49e06]">
+      <View className="h-full bg-[#1a3788]">
         <View className="container mt-28 h-full w-full bg-white rounded-t-3xl">
-          
+          <View className="items-center">
+            <Text className="text-3xl font-bold mt-2">Login</Text>
+            <Text className="text-gray-400 mt-1">Sign in to continue...</Text>
+          </View>
         </View>
       </View>
     </View>
